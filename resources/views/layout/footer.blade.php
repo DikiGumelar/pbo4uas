b275798d239fb681796d363fdf9d7d965752988c
dickigumelar <dikigmlr@gmail.com>
2021-07-13 00:35:05 +0700

    UASPBO4DIKI commit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,10 @@
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; UAS PBO4 2021 <a href="https://adminlte.io">DICKI GUMELAR</a>.</strong> All rights reserved.
+  </footer>
